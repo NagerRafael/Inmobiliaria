bce08c905d9ff364e676dfe67efafaf40f0c0d08
Rafael Naranjo <rafael.naranjo@sasf.net>
2023-03-28 10:49:22 -0500

    Creación del Diagrama Entidad-Relación, instalación del composer.json

--- DB.sql
+++ DB.sql
@@ -1,6 +1,5 @@
-DROP TABLE IF EXISTS "paquete" CASCADE;
-DROP TABLE IF EXISTS "destinos" CASCADE;
-DROP TABLE IF EXISTS "ruta" CASCADE;
+DROP TABLE IF EXISTS "peticion" CASCADE;
+DROP TABLE IF EXISTS "edificio" CASCADE;
 DROP TABLE IF EXISTS "usuario" CASCADE;
 
 CREATE TABLE "usuario" (
@@ -16,42 +15,32 @@ CREATE TABLE "usuario" (
   	PRIMARY KEY ("id_usuario")
 );
 
-CREATE TABLE "ruta" (
-  "id_ruta" serial,
-  "id_repartidor" integer not null,
-  "paquetes" json not null,
+CREATE TABLE "edificio" (
+  "id_edificio" serial,
+  "condicion" varchar(50) not null,
+  "costo" numeric(6) not null,
+  "id_asesor" integer,
+  "estado" varchar(1),
+  CONSTRAINT "PK_edificio"
+	PRIMARY KEY ("id_edificio"),
+  CONSTRAINT "FK_edificio_id_asesor"
+    FOREIGN KEY ("id_asesor") REFERENCES "usuario"("id_usuario")
+);
+
+
+CREATE TABLE "peticion" (
+  "id_peticion" serial,
+  "id_usuario" integer not null,
+  "id_edificio" integer not null,
   "condicion" varchar(50) not null,
   "estado" varchar(1),
-  CONSTRAINT "PK_ruta"
-	PRIMARY KEY ("id_ruta"),
-  CONSTRAINT "FK_ruta_id_repartidor"
-    FOREIGN KEY ("id_repartidor") REFERENCES "usuario"("id_usuario")
+  CONSTRAINT "PK_peticion"
+	PRIMARY KEY ("id_peticion"),
+  CONSTRAINT "FK_peticion_id_edificio"
+    FOREIGN KEY ("id_edificio") REFERENCES "edificio"("id_edificio"),
+  CONSTRAINT "FK_peticion_id_usuario"
+    FOREIGN KEY ("id_usuario") REFERENCES "usuario"("id_usuario")
 );
-
-CREATE TABLE "destinos" (
-  "id_destinos" serial,
-  "destinos" json,
-  "estado" varchar(1),
-  CONSTRAINT "PK_destinos"
-	PRIMARY KEY ("id_destinos")
-);
-
-CREATE TABLE "paquete" (
-  "id_paquete" serial,
-  "peso" numeric(2,1) not null,
-  "destino" varchar(100),
-  "emisor" integer,
-  "receptor" integer,
-  "tipo" varchar(50),
-  "id_ruta" integer,
-  "condicion" varchar(50),
-  "estado" varchar(1),
-  CONSTRAINT "PK_paquete"
-	PRIMARY KEY ("id_paquete"),
-  CONSTRAINT "FK_Paquete_id_ruta"
-    FOREIGN KEY ("id_ruta") REFERENCES "ruta"("id_ruta")
-);
-
 
 INSERT INTO usuario (nombre, apellido, correo, clave, tipo, roles, estado)
     VALUES ('admin', 'admin', 'admin@gmail.com', '$2y$13$p6GUK.ECGHLu8bQtEn3o6uwMF6lqtPB/0.RoQD7tm/Vjdk6fR/j6e', 'ADMIN','["ROLE_ADMIN"]','A');
@@ -59,11 +48,8 @@ INSERT INTO usuario (nombre, apellido, correo, clave, tipo, roles, estado)
 SELECT * FROM public."usuario"
 ORDER BY id_usuario ASC; 
 
-SELECT * FROM public."ruta"
-ORDER BY id_ruta ASC; 
+SELECT * FROM public."peticion"
+ORDER BY id_peticion ASC; 
 
-SELECT * FROM public."destinos"
-ORDER BY id_destinos ASC; 
-
-SELECT * FROM public."paquete"
-ORDER BY id_paquete ASC; 
+SELECT * FROM public."edificio"
+ORDER BY id_edificio ASC; 
